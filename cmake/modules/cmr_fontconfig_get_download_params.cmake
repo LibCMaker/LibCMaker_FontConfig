bf54294a009e55cf0dcbb2aa67a5619485b6b768
NikitaFeodonit <nfeodonit@yandex.com>
2018-05-25 15:58:18 +0300

    Remove support for version 2.11.1.1.

--- cmake/modules/cmr_fontconfig_get_download_params.cmake
+++ cmake/modules/cmr_fontconfig_get_download_params.cmake
@@ -30,19 +30,14 @@ function(cmr_fontconfig_get_download_params
 
   set(lib_base_url "https://www.freedesktop.org/software/fontconfig/release")
 
-  if(version VERSION_EQUAL "2.11.1.1")
-    # FontConfig v2.11.1 with CMake scripts.
-    set(lib_sha
-      "9255f2e71dc802d01fb90fde7eb8826cfcc2728fe1ea5a5f3101197fd373ae8e")
-  endif()
   if(version VERSION_EQUAL "2.11.95")
     set(lib_sha
       "7b165eee7aa22dcc1557db56f58d905b6a14b32f9701c79427452474375b4c89")
   endif()
-  if(version VERSION_EQUAL "2.12.91")
-    set(lib_sha
-      "4d56b2f88bc99a2e0e2532e1e047f60c7977a2f939d81e76aca0d146b9a6457e")
-  endif()
+#  if(version VERSION_EQUAL "2.12.91")
+#    set(lib_sha
+#      "4d56b2f88bc99a2e0e2532e1e047f60c7977a2f939d81e76aca0d146b9a6457e")
+#  endif()
   if(version VERSION_EQUAL "2.13.0")
     set(lib_sha
       "91dde8492155b7f34bb95079e79be92f1df353fcc682c19be90762fd3e12eeb9")
@@ -57,19 +52,6 @@ function(cmr_fontconfig_get_download_params
   set(lib_src_name "fontconfig-${major}.${minor}.${patch}")
   set(lib_tar_file_name "${lib_src_name}.tar.bz2")
   set(lib_url "${lib_base_url}/${lib_tar_file_name}")
-
-  # FontConfig v2.11.1 with CMake scripts.
-  if(version VERSION_EQUAL "2.11.1.1")
-    # FontConfig v2.11.1 with CMake scripts.
-    set(lib_base_url
-      "https://bitbucket.org/libcmaker_downloads/fontconfig_sources/raw")
-    set(lib_src_commit
-      "96a06b0881bebc5f02431587e0b551e7f234d098")
-    
-    set(lib_src_name "fontconfig-${major}.${minor}.${patch}-cmakeports")
-    set(lib_tar_file_name "${lib_src_name}.tar.bz2")
-    set(lib_url "${lib_base_url}/${lib_src_commit}/${lib_tar_file_name}")
-  endif()
 
   set(${out_url} "${lib_url}" PARENT_SCOPE)
   set(${out_sha} "${lib_sha}" PARENT_SCOPE)
