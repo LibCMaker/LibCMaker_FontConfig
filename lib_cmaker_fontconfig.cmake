bb85dddc4bc8f6da4afaec9be0dcc1513f57bf25
NikitaFeodonit <nfeodonit@yandex.com>
2018-08-08 12:32:43 +0300

    Add the LANGUAGES to lib_cmaker_*().

--- lib_cmaker_fontconfig.cmake
+++ lib_cmaker_fontconfig.cmake
@@ -110,6 +110,7 @@ function(lib_cmaker_fontconfig)
   cmr_lib_cmaker_main(
     NAME          ${cmr_lib_NAME}
     VERSION       ${arg_VERSION}
+    LANGUAGES     CXX C
     BASE_DIR      ${lcm_${cmr_lib_NAME}_SRC_DIR}
     DOWNLOAD_DIR  ${arg_DOWNLOAD_DIR}
     UNPACKED_DIR  ${arg_UNPACKED_DIR}
