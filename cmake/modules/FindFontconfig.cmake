c08b5e3ceda27310964b205c6db68a358970ca8f
NikitaFeodonit <nfeodonit@yandex.com>
2019-10-31 07:34:58 +0300

    Rename FindFontConfig.cmake to FindFontconfig.cmake

--- cmake/modules/FindFontconfig.cmake
+++ cmake/modules/FindFontconfig.cmake
@@ -45,7 +45,7 @@ find_library(FONTCONFIG_LIBRARY
 # handle the QUIETLY and REQUIRED arguments and set FONTCONFIG_FOUND to TRUE
 # if all listed variables are TRUE.
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(FontConfig DEFAULT_MSG
+find_package_handle_standard_args(Fontconfig DEFAULT_MSG
   FONTCONFIG_LIBRARY  FONTCONFIG_INCLUDE_DIR)
 
 if(FONTCONFIG_FOUND)
